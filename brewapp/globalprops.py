284c139419db4f32a0d4584264dd816790286cc6
Manuel83 <manuel.fritsch@gmail.com>
2015-06-24 22:34:07 +0200

    fix

--- brewapp/globalprops.py
+++ brewapp/globalprops.py
@@ -26,7 +26,7 @@ pidD=5
 
 ## hysteresis parameter in degrees celsius
 ## For exmpale. If current temperatur is more than 2 degrees below the traget temp turn the heating 100% on
-hysteresis_min=2
+hysteresis_min=4
 
 
 
