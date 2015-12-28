11a0eed220f04544b4a9ef334aa1e493dc68e78c
Manuel83 <manuel.fritsch@gmail.com>
2015-12-28 14:20:03 +0100

    try catch

--- brewapp/base/thermometer.py
+++ brewapp/base/thermometer.py
@@ -3,30 +3,32 @@ from brewapp import app, db, socketio
 from model import *
 from thread import start_new_thread
 import time
+from brewapp import brewjob, brewinit
 
 ## Method to read the temperatur
 def tempData1Wire(tempSensorId):
-
-    ## Test Mode
-    if (app.testMode == True):
-        pipe = Popen(["cat","w1_slave"], stdout=PIPE)
-    ## GPIO Mode
-    else:
-        pipe = Popen(["cat","/sys/bus/w1/devices/w1_bus_master1/" + tempSensorId + "/w1_slave"], stdout=PIPE)
-    result = pipe.communicate()[0]
-
-    ## parse the file
-    if (result.split('\n')[0].split(' ')[11] == "YES"):
-        temp_C = float(result.split("=")[-1])/1000 # temp in Celcius
-    else:
-        temp_C = -99 #bad temp reading
+    try:
+        ## Test Mode
+        if (app.testMode == True):
+            pipe = Popen(["cat","w1_slave"], stdout=PIPE)
+            ## GPIO Mod
+        else:
+            pipe = Popen(["cat","/sys/bus/w1/devices/w1_bus_master1/" + tempSensorId + "/w1_slave"], stdout=PIPE)
+        result = pipe.communicate()[0]
+        ## parse the file
+        if (result.split('\n')[0].split(' ')[11] == "YES"):
+            temp_C = float(result.split("=")[-1])/1000 # temp in Celcius
+        else:
+            temp_C = -99 #bad temp reading
+    except:
+        temp_C = -1
 
     return temp_C
 
-
+@brewjob(key="tempjob")
 def readTemp():
-    while True:
-
+    app.brewapp_chartdata["temp1"] = []
+    while app.brewapp_jobstate["tempjob"]:
         app.brewapp_temperature["value1"] = tempData1Wire("ABC")
         t = Temperature()
         t.time = datetime.utcnow()
@@ -38,6 +40,7 @@ def readTemp():
         print "READ TEMP", t.value1
         socketio.emit('chart_update', update, namespace ='/brew')
         time.sleep( 5 )
+    print "TEMP JOB STOPPED"
 
 def clearData():
     Temperature.query.delete()
@@ -45,18 +48,12 @@ def clearData():
     app.brewapp_chartdata = {}
     app.brewapp_chartdata["temp1"] = []
 
+
+@brewinit()
 def loadData():
     temps = db.session.query(Temperature).all()
     x = 'temp1'
     app.brewapp_chartdata[x] = []
-
-    print len(temps)
     for t in temps:
         x = 'temp1'
         app.brewapp_chartdata[x] += [[t.to_unixTime(), t.value1]]
-
-def job():
-    loadData()
-    start_new_thread(readTemp,())
-
-app.brewapp_jobs.append(job)
