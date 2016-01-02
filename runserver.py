745cccc041cce5b4c4f315133ec5d8378902b009
Manuel83 <manuel.fritsch@gmail.com>
2016-01-02 12:47:41 +0100

    #!/usr/bin/env python added

--- runserver.py
+++ runserver.py
@@ -1,14 +1,10 @@
+#!/usr/bin/env python
+
 from flask import Flask
 from brewapp import app, socketio
 from flask.ext.socketio import SocketIO, emit
-#app = Flask(__name__)
-
-#@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
 
 
 if __name__ == '__main__':
     app.debug = True
-    
     socketio.run(app, host='0.0.0.0', use_reloader=False)
