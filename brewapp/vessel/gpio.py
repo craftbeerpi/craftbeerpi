c33c16bbcb2bc9c0136e3031922b7323f204a313
Manuel83 <manuel.fritsch@gmail.com>
2016-01-02 14:32:11 +0100

    fox

--- brewapp/vessel/gpio.py
+++ brewapp/vessel/gpio.py
@@ -15,11 +15,12 @@ def initGPIO():
         call(["modprobe", "w1-gpio"])
         call(["modprobe", "w1-therm"])
         print "###### SETUP GPIO 2 #######"
+        GPIO.setup(23, GPIO.OUT)
         for vid in app.brewapp_vessel:
             if(app.brewapp_vessel[vid]["heater"]["gpio"] != None):
                 print "SETUP GPIO HEATER", app.brewapp_vessel[vid]["heater"]["gpio"]
                 GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
-                GPIO.output(app.brewapp_vessel[vid]["agitator"]["gpio"], 0)
+                GPIO.output(app.brewapp_vessel[vid]["agitator"]["gpio"], 1)
             if(app.brewapp_vessel[vid]["agitator"]["gpio"] != None):
                 print "SETUP GPIO AGITATOR", app.brewapp_vessel[vid]["agitator"]["gpio"]
                 GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
