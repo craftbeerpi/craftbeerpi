cf41982199f18018df003f30b7fd67bdc325a8d9
Manuel83 <manuel.fritsch@gmail.com>
2016-01-02 14:03:56 +0100

    fix

--- brewapp/vessel/gpio.py
+++ brewapp/vessel/gpio.py
@@ -16,10 +16,10 @@ def initGPIO():
         for vid in app.brewapp_vessel:
             if(app.brewapp_vessel[vid]["heater"]["gpio"] != None):
                 print "SETUP GPIO HEATER", app.brewapp_vessel[vid]["heater"]["gpio"]
-                #GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
+                GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
             if(app.brewapp_vessel[vid]["agitator"]["gpio"] != None):
                 print "SETUP GPIO AGITATOR", app.brewapp_vessel[vid]["agitator"]["gpio"]
-                #GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
+                GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
         app.brewapp_gpio = True
     except ImportError:
         print  "     -->GPIO SETUP FAILED"
