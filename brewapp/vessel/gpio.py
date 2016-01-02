61d6ebeb3f17137f7a8127f04a760adccb54c8d5
Manuel83 <manuel.fritsch@gmail.com>
2016-01-02 14:08:37 +0100

    fix

--- brewapp/vessel/gpio.py
+++ brewapp/vessel/gpio.py
@@ -16,7 +16,7 @@ def initGPIO():
             if(app.brewapp_vessel[vid]["heater"]["gpio"] != None):
                 print "SETUP GPIO HEATER", app.brewapp_vessel[vid]["heater"]["gpio"]
                 GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
-                GPIO.output(gpio, 1)
+                GPIO.output(app.brewapp_vessel[vid]["agitator"]["gpio"], 1)
             if(app.brewapp_vessel[vid]["agitator"]["gpio"] != None):
                 print "SETUP GPIO AGITATOR", app.brewapp_vessel[vid]["agitator"]["gpio"]
                 GPIO.setup(app.brewapp_vessel[vid]["agitator"]["gpio"], GPIO.OUT)
