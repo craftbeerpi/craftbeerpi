225844c6e20762d940584bcf3d9c114baed7f03f
Manuel83 <manuel.fritsch@gmail.com>
2016-01-10 23:14:44 +0100

    fix

--- brewapp/__init__.py
+++ brewapp/__init__.py
@@ -17,7 +17,7 @@ socketio = SocketIO(app)
 #logging.getLogger('sqlalchemy.engine').addHandler(RotatingFileHandler('sqllog.log', maxBytes=10000, backupCount=1))
 #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+handler = RotatingFileHandler('./foo.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 
