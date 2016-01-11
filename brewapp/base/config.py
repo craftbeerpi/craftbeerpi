1346d20ae62e46e7daa8cb4a0abb60a4b340efd6
Manuel83 <manuel.fritsch@gmail.com>
2016-01-12 00:14:40 +0100

    fix

--- brewapp/base/config.py
+++ brewapp/base/config.py
@@ -8,6 +8,7 @@ app.brewapp_jobstate = {}
 app.brewapp_current_step = None
 app.brewapp_button = {"next": 23, "reset": 24}
 app.brewapp_kettle_state = {}
+app.brewapp_kettle = {}
 app.brewapp_kettle_temps_log = {}
 app.brewapp_kettle_automatic = {}
 app.brewapp_pid_state =  {}
