7a428d581001ae38fc3affca2fa3a6f4565feded
Manuel83 <manuel.fritsch@gmail.com>
2016-01-11 22:02:38 +0100

    fix

--- brewapp/base/gpio.py
+++ brewapp/base/gpio.py
@@ -12,31 +12,33 @@ except:
 def initGPIO():
     app.logger.info("## Init GIPO")
     try:
-        call(["modprobe", "w1-gpio"])
-        call(["modprobe", "w1-therm"])
+        #call(["modprobe", "w1-gpio"])
+        #call(["modprobe", "w1-therm"])
         #print "###### SETUP GPIO 2 #######"
         for vid in app.brewapp_kettle_state:
             print vid
             if(app.brewapp_kettle_state[vid]["heater"]["gpio"] != None):
-                print "HEATER" app.brewapp_kettle_state[vid]["heater"]["gpio"]
-                app.logger.info("SETUP GPIO HEATER: " + app.brewapp_kettle_state[vid]["heater"]["gpio"])
+                print "HEATER", app.brewapp_kettle_state[vid]["heater"]["gpio"]
+                app.logger.info("SETUP GPIO HEATER: " + str(app.brewapp_kettle_state[vid]["heater"]["gpio"]))
                 #GPIO.setup(int(app.brewapp_kettle[vid]["heater"]["gpio"]), GPIO.OUT)
                 #GPIO.output(app.brewapp_kettle[vid]["heater"]["gpio"], 1)
             if(app.brewapp_kettle_state[vid]["agitator"]["gpio"] != None):
-                print "AGITATOR" app.brewapp_kettle_state[vid]["agitator"]["gpio"]
-                app.logger.info("SETUP GPIO AGITATOR" + app.brewapp_kettle_state[vid]["agitator"]["gpio"])
+                print "AGITATOR", app.brewapp_kettle_state[vid]["agitator"]["gpio"]
+                app.logger.info("SETUP GPIO AGITATOR" + str(app.brewapp_kettle_state[vid]["agitator"]["gpio"]))
                 #GPIO.setup(app.brewapp_kettle[vid]["agitator"]["gpio"], GPIO.OUT)
                 #GPIO.output(app.brewapp_kettle[vid]["agitator"]["gpio"], 1)
         initHardwareButton()
         app.brewapp_gpio = True
         app.logger.info("ALL GPIO INITIALIZED")
     except Exception as e:
+        print "GPIO ERRO"
         print e
         app.logger.error("SETUP GPIO FAILD " + str(e))
         app.brewapp_gpio = False
 
 def initHardwareButton():
     if(app.brewapp_button != None):
+        print "SETUP BUTTON"
         GPIO.setup(app.brewapp_button["next"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
         GPIO.add_event_detect(app.brewapp_button["next"], GPIO.RISING, callback=nextStep, bouncetime=300)
 
