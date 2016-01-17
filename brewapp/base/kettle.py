2a70fcb7f5b0bf5ec250304cb616dc16687b2de8
Manuel83 <manuel.fritsch@gmail.com>
2016-01-17 18:53:17 +0100

    fix

--- brewapp/base/kettle.py
+++ brewapp/base/kettle.py
@@ -6,7 +6,7 @@ from brewapp import app, socketio
 from views import base
 from w1_thermometer import *
 from brewapp import manager
-from brewapp.base.pid import *
+from brewapp.base.pid.pidbase import *
 
 ## HTTP METHODS
 @base.route('/gpio', methods=['PUT'])
