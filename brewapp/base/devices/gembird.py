976abb98e0c809ea14aab9391493855503964dfd
Manuel83 <manuel.fritsch@gmail.com>
2016-01-18 20:26:21 +0100

    fix

--- brewapp/base/devices/gembird.py
+++ brewapp/base/devices/gembird.py
@@ -7,7 +7,7 @@ class Gemini(HardwareBase):
 
     def getDevices(self):
         gpio = []
-        for i in range(1, 6):
+        for i in range(1, 4):
             gpio.append("SOCKET"+str(i))
         return gpio
 
