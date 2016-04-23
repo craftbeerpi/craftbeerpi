a9fbf9989016715e0903eb259f6f5cb89350a72b
Manuel83 <manuel.fritsch@gmail.com>
2016-04-23 19:33:39 +0200

    change to utc

--- brewapp/base/kettle.py
+++ brewapp/base/kettle.py
@@ -133,7 +133,7 @@ def readKettleTemp():
         else:
             app.brewapp_kettle_state[vid]["temp"] = temp
 
-            timestamp = int((datetime.now() - datetime(1970,1,1)).total_seconds())*1000
+            timestamp = int((datetime.utcnow() - datetime(1970,1,1)).total_seconds())*1000
         app.brewapp_kettle_temps_log[vid] += [[timestamp, app.brewapp_kettle_state[vid]["temp"] ]]
 
     socketio.emit('kettle_state_update', app.brewapp_kettle_state, namespace ='/brew')
