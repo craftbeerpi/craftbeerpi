d62837c9f86abc82982b12749bdf82b42482dfc4
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-12-08 20:23:27 +0100

    Add virtual sensors

--- brewapp/base/thermometer/w1_thermometer2.py
+++ brewapp/base/thermometer/w1_thermometer2.py
@@ -1,34 +1,48 @@
-import os, re
-from subprocess import Popen, PIPE, call
-from random import randint, uniform
+import os
+import re
+import sys
 from brewapp import app
-from decimal import Decimal, ROUND_HALF_UP
 from subprocess import call
 
 
 class OneWireThermometer2(object):
+    AVERAGE_SENSOR_ID = "average"
+    MAX_SENSOR_ID = "maximum"
 
     def init(self):
         try:
             call(["modprobe", "w1-gpio"])
             call(["modprobe", "w1-therm"])
         except Exception as e:
-            app.logger.error("Faild to initialize 1 wire thermometert ERROR: " + str(e))
+            app.logger.error(
+                "Failed to initialize 1 wire thermometer ERROR: " + str(e))
 
     def getSensors(self):
         try:
-            arr = []
-            for dirname in os.listdir('/sys/bus/w1/devices'):
-                if(dirname.startswith("28") or dirname.startswith("10")):
-                    arr.append(dirname)
+            arr = self.__listW1Sensors()
+            arr.extend([self.AVERAGE_SENSOR_ID, self.MAX_SENSOR_ID])
             return arr
         except:
-            return ["1WDummySensor1","1W    DummySensor2"]
+            return ["1WDummySensor1", "1WDummySensor2"]
 
     def readTemp(self, tempSensorId):
+        if tempSensorId == self.AVERAGE_SENSOR_ID:
+            return self.__getAverageTemp()
+        elif tempSensorId == self.MAX_SENSOR_ID:
+            return self.__getMaxTemp()
+        else:
+            return self.__getSensorValue(tempSensorId)
 
+    def __listW1Sensors(self):
+        arr = []
+        for dirname in os.listdir('/sys/bus/w1/devices'):
+            if(dirname.startswith("28") or dirname.startswith("10")):
+                arr.append(dirname)
+        return arr
+
+    def __getSensorValue(self, tempSensorId):
         value = None
-        path = "/sys/bus/w1/devices/w1_bus_master1/"+tempSensorId+"/w1_slave"
+        path = "/sys/bus/w1/devices/w1_bus_master1/" + tempSensorId + "/w1_slave"
 
         try:
             f = open(path, "r")
@@ -39,8 +53,30 @@ class OneWireThermometer2(object):
                 if m:
                     value = float(m.group(2)) / 1000.0
             f.close()
-        except (Exception), e:
+        except Exception as e:
             app.logger.warning("Read temp failed " + str(e))
+
+        return value
+
+    def __getAverageTemp(self):
+        value = 0
+        count = 0
+        for sensor in self.__listW1Sensors():
+            value += self.__getSensorValue(sensor)
+            count += 1
+
+        if count == 0:
+            return 0
+        else:
+            return value / count
+
+    def __getMaxTemp(self):
+        value = sys.float_info.min
+
+        for sensor in self.__listW1Sensors():
+            tmp = self.__getSensorValue(sensor)
+            if tmp > value:
+                value = tmp
 
         return value
 
