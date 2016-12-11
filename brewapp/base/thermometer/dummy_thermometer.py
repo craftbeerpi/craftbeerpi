177175594da678257f18cf37a09c9c2d617f5252
Manuel83 <manuel.fritsch@gmail.com>
2016-12-11 10:03:34 +0100

    fix

--- brewapp/base/thermometer/dummy_thermometer.py
+++ brewapp/base/thermometer/dummy_thermometer.py
@@ -9,20 +9,24 @@ from flask import Blueprint, render_template, json, request
 from brewapp.base.model import *
 from brewapp import app, socketio
 
+app.cbp["TEMP"] = {"DummySensor1": 20.99, "DummySensor2": 20, "DummySensor3": 20}
+
 class DummyThermometer(object):
 
     def init(self):
-        pass
-        app.cbp["TEMP"] = {"DummySensor1": 20.99, "DummySensor2": 20, "DummySensor3": 20}
+        print "INIT"
+
 
     def getSensors(self):
         return ["DummySensor1","DummySensor2","DummySensor3"]
 
     def readTemp(self, tempSensorId):
-        if app.cbp["TEMP"][tempSensorId]:
+
+        try:
             return app.cbp["TEMP"][tempSensorId]
-        else:
-            return -99
+        except Exception as e:
+            print e
+            return None
             
 @app.route('/api/test/temps', methods=['GET'])
 def dummy_temps():
@@ -31,5 +35,6 @@ def dummy_temps():
 @app.route('/set', methods=["POST"])
 def test_set():
     dataDict = json.loads(request.data)
+    print app.cbp
     app.cbp["TEMP"][dataDict["id"]] = dataDict.get("value", None)
     return ('', 204)
