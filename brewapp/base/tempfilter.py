217f3b1f17de81c1a80b9ece13c77152e3f954b5
Stefan Hirschmann <st.hirschmann@t-online.de>
2017-01-20 19:24:56 +0100

    Add logging capability

--- brewapp/base/tempfilter.py
+++ brewapp/base/tempfilter.py
@@ -1,9 +1,11 @@
+import logging
 from collections import deque
 
 
 class TemperatureFilter(object):
 
     def __init__(self, maxViableDeviation=4):
+        self._logger = logging.getLogger(type(self).__name__)
         self.__deque = deque(maxlen=5)
         self.__maxViableDeviation = maxViableDeviation
         self.__lastKnownGood = 0
@@ -13,9 +15,9 @@ class TemperatureFilter(object):
         median = sorted(self.__deque)[len(self.__deque)//2]
         deviation = abs(median - temperature)
 
-        print("deq:" + str(self.__deque))
-        print("med:" + str(median))
-        print("dev:" + str(deviation))
+        self._logger.debug("deq: " + str(self.__deque))
+        self._logger.debug("med: " + str(median))
+        self._logger.debug("dev: " + str(deviation))
 
         if deviation > self.__maxViableDeviation:
             return self.__lastKnownGood
